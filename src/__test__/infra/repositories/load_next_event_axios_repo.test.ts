13852ec22fb1061df30a5e3cf54a34766d5850a6
Junior Marques <brq.calmarques@voegol.com>
2024-11-02 17:55:42 -0300

    feat: ensure LoadNextEventRepositoryAxios request with correct method

--- src/__test__/infra/repositories/load_next_event_axios_repo.test.ts
+++ src/__test__/infra/repositories/load_next_event_axios_repo.test.ts
@@ -5,7 +5,7 @@ import {
   LoadNextEventRepository,
 } from '../../../domain/repository/load_next_repo';
 import {NextEventEntity} from '../../../domain/entities/next_event_';
-import {faker} from '@faker-js/faker';
+import {anyString} from '../../helpers/fakes';
 
 interface IHttpClient<T = any> {
   get(url: string): Promise<T>;
@@ -14,19 +14,25 @@ interface IHttpClient<T = any> {
 class HttpClientSpy implements IHttpClient {
   callsCount = 0;
   method = '';
+  url = '';
 
   async get(url: string): Promise<any> {
     this.method = 'get';
+    this.url = url;
     this.callsCount++;
   }
 }
 
 class LoadNextEventRepositoryAxios implements LoadNextEventRepository {
-  constructor(private readonly httpClient: IHttpClient<NextEventEntity>) {}
+  constructor(
+    private readonly httpClient: IHttpClient<NextEventEntity>,
+    private readonly url: string,
+  ) {}
   async loadNextEvent({
     groupId,
   }: loadNextEventParams): Promise<NextEventEntity> {
-    await this.httpClient.get('');
+    const url = this.url.replace(':groupId', groupId);
+    await this.httpClient.get(url);
     return {} as NextEventEntity;
   }
 }
@@ -34,11 +40,23 @@ class LoadNextEventRepositoryAxios implements LoadNextEventRepository {
 describe('LoadNextEventRepositoryAxios', () => {
   it('should request with correct method', async () => {
     const httpClient = new HttpClientSpy();
-    const groupId = faker.string.uuid();
-    const sut = new LoadNextEventRepositoryAxios(httpClient);
+    const groupId = anyString();
+    const sut = new LoadNextEventRepositoryAxios(httpClient, '');
     await sut.loadNextEvent({groupId});
 
     expect(httpClient.method).toBe('get');
     expect(httpClient.callsCount).toBe(1);
+  });
+
+  it('should request with correct url', async () => {
+    const groupId = anyString();
+    const url = `https://domain.com/api/groups/:groupId/next_events`;
+    const httpClient = new HttpClientSpy();
+    const sut = new LoadNextEventRepositoryAxios(httpClient, url);
+    await sut.loadNextEvent({groupId});
+
+    expect(httpClient.url).toBe(
+      `https://domain.com/api/groups/${groupId}/next_events`,
+    );
   });
 });
