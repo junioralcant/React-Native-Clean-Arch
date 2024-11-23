40d30221ce1253aece89cc14cb587cfad0b60fcc
Junior Marques <brq.calmarques@voegol.com>
2024-11-23 11:25:27 -0300

    feat: ensure LoadNextEventRepositoryAxios return NextEvent on 200

--- src/__test__/infra/repositories/load_next_event_axios_repo.test.ts
+++ src/__test__/infra/repositories/load_next_event_axios_repo.test.ts
@@ -6,9 +6,15 @@ import {
 } from '../../../domain/repository/load_next_repo';
 import {NextEventEntity} from '../../../domain/entities/next_event_';
 import {anyString} from '../../helpers/fakes';
+import {ne} from '@faker-js/faker';
+import {NextEventPlayerEntity} from '../../../domain/entities/next_event_player';
+
+export type HttpResponse<TData = any> = {
+  data: TData;
+};
 
 interface IHttpClient<T = any> {
-  get(url: string, headers?: any): Promise<T>;
+  get(url: string, headers?: any): Promise<HttpResponse<T>>;
 }
 
 class HttpClientSpy implements IHttpClient {
@@ -16,12 +22,14 @@ class HttpClientSpy implements IHttpClient {
   method = '';
   url = '';
   headers: Record<string, string> = {};
+  response = {};
 
-  async get(url: string, headers?: any): Promise<any> {
+  async get(url: string, headers?: any): Promise<HttpResponse> {
     this.headers = headers;
     this.method = 'get';
     this.url = url;
     this.callsCount++;
+    return {data: this.response};
   }
 }
 
@@ -38,8 +46,21 @@ class LoadNextEventRepositoryAxios implements LoadNextEventRepository {
       'content-type': 'application/json',
       accept: 'application/json',
     };
-    await this.httpClient.get(url, headers);
-    return {} as NextEventEntity;
+    const {data: response} = await this.httpClient.get(url, headers);
+    return new NextEventEntity({
+      date: response.date,
+      groupName: response.groupName,
+      players: response.players.map(player =>
+        NextEventPlayerEntity.create({
+          id: player.id,
+          name: player.name,
+          isConfirmed: player.isConfirmed,
+          photo: player.photo,
+          position: player.position,
+          confirmationDate: player?.confirmationDate,
+        }),
+      ),
+    });
   }
 }
 
@@ -53,6 +74,25 @@ describe('LoadNextEventRepositoryAxios', () => {
     groupId = anyString();
     url = `https://domain.com/api/groups/:groupId/next_events`;
     httpClient = new HttpClientSpy();
+    httpClient.response = {
+      date: '2024-01-01T10:30',
+      groupName: 'any_name',
+      players: [
+        {
+          id: 'id 1',
+          name: 'name 1',
+          isConfirmed: true,
+        },
+        {
+          id: 'id 2',
+          name: 'name 2',
+          isConfirmed: false,
+          position: 'position 2',
+          confirmationDate: '2024-01-01T10:30',
+          photo: 'photo 2',
+        },
+      ],
+    };
     sut = new LoadNextEventRepositoryAxios(httpClient, url);
   });
 
@@ -73,5 +113,23 @@ describe('LoadNextEventRepositoryAxios', () => {
     await sut.loadNextEvent({groupId});
     expect(httpClient?.headers['content-type']).toBe(`application/json`);
     expect(httpClient?.headers['accept']).toBe(`application/json`);
+  });
+
+  it('should request NextEvent on 200', async () => {
+    const event = await sut.loadNextEvent({groupId});
+    expect(event.groupName).toBe('any_name');
+    expect(event.date).toBe('2024-01-01T10:30');
+
+    expect(event.players[0].id).toBe('id 1');
+    expect(event.players[0].name).toBe('name 1');
+    expect(event.players[0].isConfirmed).toBe(true);
+
+    expect(event.players[1].id).toBe('id 2');
+    expect(event.players[1].name).toBe('name 2');
+    expect(event.players[1].position).toBe('position 2');
+    expect(event.players[1].photo).toBe('photo 2');
+    expect(event.players[1].confirmationDate).toBe('2024-01-01T10:30');
+
+    expect(event.players[1].isConfirmed).toBe(false);
   });
 });
