c2d947da1883a923005232712c8d977f8fc44848
Junior Marques <brq.calmarques@voegol.com>
2024-12-29 09:58:20 -0300

    move sut to create in beforeEach

--- src/__test__/infra/api/clients/http_client.test.ts
+++ src/__test__/infra/api/clients/http_client.test.ts
@@ -1,7 +1,4 @@
 import {expect, it, describe, beforeEach} from '@jest/globals';
-import {anyString} from '../../../helpers/fakes';
-import {LoadNextEventApiRepository} from '../../../../infra/api/repositories/load_next_event_api_repo';
-import {HttpGetClientSpy} from '../clients/http_get_client_spy';
 import {ClientSpy} from './client_spy';
 
 class HttpClient {
@@ -12,11 +9,19 @@ class HttpClient {
   }
 }
 describe('HttpClient', () => {
-  it('should request with correct method', async () => {
-    const client = new ClientSpy();
-    const sut = new HttpClient(client);
-    await sut.get();
-    expect(client.method).toBe('get');
-    expect(client.callsCount).toBe(1);
+  let client: ClientSpy;
+  let sut: HttpClient;
+
+  beforeEach(() => {
+    client = new ClientSpy();
+    sut = new HttpClient(client);
+  });
+
+  describe('get', () => {
+    it('should request with correct method', async () => {
+      await sut.get();
+      expect(client.method).toBe('get');
+      expect(client.callsCount).toBe(1);
+    });
   });
 });
