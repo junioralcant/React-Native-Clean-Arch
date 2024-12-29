b35fbf57d05900cb4b83d7f4cec8041cdedca054
Junior Marques <brq.calmarques@voegol.com>
2024-12-29 10:03:39 -0300

    ensure HttpClient request with correct url

--- src/__test__/infra/api/clients/http_client.test.ts
+++ src/__test__/infra/api/clients/http_client.test.ts
@@ -1,27 +1,35 @@
 import {expect, it, describe, beforeEach} from '@jest/globals';
 import {ClientSpy} from './client_spy';
+import {anyString} from '../../../helpers/fakes';
 
 class HttpClient {
   constructor(private readonly client: ClientSpy) {}
 
-  async get() {
-    this.client.get('');
+  async get({url}: {url: string}) {
+    this.client.get(url);
   }
 }
 describe('HttpClient', () => {
   let client: ClientSpy;
   let sut: HttpClient;
+  let url: string;
 
   beforeEach(() => {
     client = new ClientSpy();
     sut = new HttpClient(client);
+    url = anyString();
   });
 
   describe('get', () => {
     it('should request with correct method', async () => {
-      await sut.get();
+      await sut.get({url});
       expect(client.method).toBe('get');
       expect(client.callsCount).toBe(1);
+    });
+
+    it('should request with correct url', async () => {
+      await sut.get({url});
+      expect(client.url).toBe(url);
     });
   });
 });
