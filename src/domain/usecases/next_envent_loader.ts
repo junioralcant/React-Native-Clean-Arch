a35399a7f1dd43f0bb263e27c72d7e35f20913a2
Junior Marques <brq.calmarques@voegol.com>
2025-03-31 19:16:27 -0300

    add correct interfact to NextEventLoaderUseCase

--- src/domain/usecases/next_envent_loader.ts
+++ src/domain/usecases/next_envent_loader.ts
@@ -1,8 +1,8 @@
 import {NextEventEntity} from '../entities/next_event_';
-import {LoadNextEventRepository} from '../repository/load_next_repo';
+import {ILoadNextEventRepository} from '../repository/load_next_repo';
 
 export class NextEventLoaderUseCase {
-  constructor(readonly repo: LoadNextEventRepository) {}
+  constructor(readonly repo: ILoadNextEventRepository) {}
 
   async execute({groupId}: {groupId: string}): Promise<NextEventEntity> {
     return await this.repo.loadNextEvent({groupId});
