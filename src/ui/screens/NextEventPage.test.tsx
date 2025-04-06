8c0cd819cdff35a6c74430dc869ebf899e62124b
Junior Marques <brq.calmarques@voegol.com>
2025-04-06 09:29:26 -0300

    ensure NextEventPage loads next event data on page init

---
+++ src/ui/screens/NextEventPage.test.tsx
@@ -0,0 +1,46 @@
+import {expect, it, describe} from '@jest/globals';
+import {render} from '@testing-library/react-native';
+import {useEffect} from 'react';
+
+import {Text} from 'react-native';
+import {anyString} from '../../__test__/helpers/fakes';
+
+interface INextEventPresenter {
+  loadNextEvent: ({groupId}: {groupId: string}) => void;
+}
+
+class NextEventPresenterSpy implements INextEventPresenter {
+  loadCallsCount = 0;
+  groupId = '';
+  loadNextEvent = ({groupId}: {groupId: string}) => {
+    this.loadCallsCount++;
+    this.groupId = groupId;
+  };
+}
+
+function NextEventPage({presenter, groupId}: NextEventPageProps) {
+  useEffect(() => {
+    presenter.loadNextEvent({groupId});
+  }, []);
+
+  return <Text>NextEventPage</Text>;
+}
+
+type NextEventPageProps = {
+  readonly presenter: INextEventPresenter;
+  readonly groupId: string;
+};
+
+const sut = ({presenter, groupId}: NextEventPageProps) => {
+  render(<NextEventPage presenter={presenter} groupId={groupId} />);
+};
+
+describe('NextEventPage', () => {
+  it('should load event data on page init', () => {
+    const presenter = new NextEventPresenterSpy();
+    const groupId = anyString();
+    sut({presenter, groupId});
+    expect(presenter.loadCallsCount).toBe(1);
+    expect(presenter.groupId).toBe(groupId);
+  });
+});
