e03a7647f30e22c7053c23ed6ae818bfd02ee9d9
Junior Marques <brq.calmarques@voegol.com>
2025-04-07 18:31:34 -0300

    ensure PlayerPosition goalkeeper position

---
+++ src/__test__/ui/components/player_position.test.tsx
@@ -0,0 +1,24 @@
+import {render, screen} from '@testing-library/react-native';
+import React from 'react';
+import {expect, it, describe} from '@jest/globals';
+import {Text} from 'react-native';
+
+const sut = ({position = 'goalkeeper'}: {position?: string}) => {
+  render(<PlayerPosition position={position} />);
+};
+
+const PlayerPosition = ({position}: {position?: string}) => {
+  return <Text>Goleiro</Text>;
+};
+
+describe('PlayerPosition', () => {
+  it('should handle goalkeeper position', () => {
+    sut({position: 'goalkeeper'});
+    expect(screen.getByText('Goleiro')).toBeTruthy();
+  });
+
+  it('should handle positionless', () => {
+    sut({position: ''});
+    expect(screen.getByText('Gandula')).toBeTruthy();
+  });
+});
