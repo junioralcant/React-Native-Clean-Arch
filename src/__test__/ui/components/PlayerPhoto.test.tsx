2e3dbc20eaffa71a6b2efd71972e808dd0cdda2d
Junior Marques <brq.calmarques@voegol.com>
2025-04-11 17:36:13 -0300

    move PlayerStatus not present initials when photo is provided

--- src/__test__/ui/components/PlayerPhoto.test.tsx
+++ src/__test__/ui/components/PlayerPhoto.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-native/extend-expect';
 import {expect, it, describe} from '@jest/globals';
 import {render, screen} from '@testing-library/react-native';
-import {Text} from 'react-native';
+import {Image, Text} from 'react-native';
 
 type PlayerPhotoProps = {
   initials: string;
@@ -12,7 +12,11 @@ export const PlayerPhoto = ({
   initials = '',
   photo = '',
 }: Partial<PlayerPhotoProps> = {}) => {
-  return <Text>{initials}</Text>;
+  return !photo ? (
+    <Text>{initials}</Text>
+  ) : (
+    <Image testID="player_photo" source={{uri: photo}} />
+  );
 };
 
 const sut = ({initials = '', photo = ''}: Partial<PlayerPhotoProps> = {}) =>
@@ -22,5 +26,13 @@ describe('PlayerStatus', () => {
   it('should present initials when photo is not provided', () => {
     sut({initials: 'AB'});
     expect(screen.getByText('AB')).toBeTruthy();
+  });
+
+  it('should not present initials when photo is provided', () => {
+    sut({photo: 'https://via.placeholder.com/150'});
+    expect(screen.queryByText('AB')).toBeFalsy();
+    expect(screen.getByTestId('player_photo').props.source.uri).toBe(
+      'https://via.placeholder.com/150',
+    );
   });
 });
