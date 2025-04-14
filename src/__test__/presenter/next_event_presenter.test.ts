d2ed9f4c30a1267a204d84897b93ab8122032940
Junior Marques <brq.calmarques@voegol.com>
2025-04-14 16:41:47 -0300

    ensure NextEventPresenter throw error if use case throws error

--- src/__test__/presenter/next_event_presenter.test.ts
+++ src/__test__/presenter/next_event_presenter.test.ts
@@ -1,4 +1,4 @@
-import {describe, it, expect} from '@jest/globals';
+import {describe, it, expect, jest} from '@jest/globals';
 import {anyString} from '../helpers/fakes';
 import {INextEventLoaderUseCase} from '../../domain/usecases/next_envent_loader';
 import {NextEventEntity} from '../../domain/entities/next_event_';
@@ -8,16 +8,18 @@ class NextEventPresenter {
     private readonly nextEventLoadedUseCase: INextEventLoaderUseCase,
   ) {}
   loadNextEvent = async ({groupId}: {groupId: string}) => {
-    this.nextEventLoadedUseCase.execute({groupId});
+    await this.nextEventLoadedUseCase.execute({groupId});
   };
 }
 
 class NextEventLoadedUseCaseSpy implements INextEventLoaderUseCase {
   callsCount = 0;
   groupId = '';
+  error?: Error;
   execute = async ({groupId}: {groupId: string}): Promise<NextEventEntity> => {
     this.callsCount++;
     this.groupId = groupId;
+
     return {
       groupName: 'test',
       date: new Date(),
@@ -26,8 +28,11 @@ class NextEventLoadedUseCaseSpy implements INextEventLoaderUseCase {
   };
 }
 
-const makeSut = () => {
-  const nextEventLoadedUseCaseSpy = new NextEventLoadedUseCaseSpy();
+const makeSut = ({
+  nextEventLoadedUseCaseSpy = new NextEventLoadedUseCaseSpy(),
+}: {
+  nextEventLoadedUseCaseSpy?: NextEventLoadedUseCaseSpy;
+} = {}) => {
   const sut = new NextEventPresenter(nextEventLoadedUseCaseSpy);
   return {sut, nextEventLoadedUseCaseSpy};
 };
@@ -39,5 +44,18 @@ describe('NextEventPresenter', () => {
     await sut.loadNextEvent({groupId});
     expect(nextEventLoadedUseCaseSpy.callsCount).toBe(1);
     expect(nextEventLoadedUseCaseSpy.groupId).toBe(groupId);
+  });
+
+  it('should throw error if use case throws error', async () => {
+    const groupId = anyString();
+    const nextEventLoadedUseCaseSpy = new NextEventLoadedUseCaseSpy();
+    jest
+      .spyOn(nextEventLoadedUseCaseSpy, 'execute')
+      .mockRejectedValue(new Error('Error message'));
+
+    const {sut} = makeSut({nextEventLoadedUseCaseSpy});
+    const response = sut.loadNextEvent({groupId});
+
+    expect(response).rejects.toThrow('Error message');
   });
 });
