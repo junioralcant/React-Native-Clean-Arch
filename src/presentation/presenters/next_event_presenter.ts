f825ebb2dda0d109d5f1f49f576fa359c6502a31
Junior Marques <brq.calmarques@voegol.com>
2025-04-18 09:19:49 -0300

    move NextEventPresenter to own file

--- src/presentation/presenters/next_event_presenter.ts
+++ src/presentation/presenters/next_event_presenter.ts
@@ -1,3 +1,7 @@
+import {NextEventEntity} from '../../domain/entities/next_event_';
+import {NextEventPlayerEntity} from '../../domain/entities/next_event_player';
+import {INextEventLoaderUseCase} from '../../domain/usecases/next_envent_loader';
+
 export interface INextEventPresenter {
   loadNextEvent({
     groupId,
@@ -61,5 +65,75 @@ export class NextEventPlayerViewModel {
     this.initials = initials ?? '';
     this.isConfirmed = isConfirmed ?? false;
     this.confirmationDate = confirmationDate ?? null;
+  }
+}
+
+export class NextEventPresenter implements INextEventPresenter {
+  constructor(
+    private readonly nextEventLoadedUseCase: INextEventLoaderUseCase,
+  ) {}
+
+  async loadNextEvent({
+    groupId,
+    isReload,
+  }: {
+    groupId: string;
+    isReload?: boolean;
+  }): Promise<NextEventViewModel> {
+    const response = await this.nextEventLoadedUseCase.execute({groupId});
+    return this.mapEventToViewModel(response);
+  }
+
+  private mapEventToViewModel(event: NextEventEntity): NextEventViewModel {
+    return {
+      doubt: event.players
+        .filter(player => !player.confirmationDate)
+        .map(this.mapPlayersToViewModel)
+        .sort((a, b) => a.name.localeCompare(b.name)),
+      goalKeepers: event.players
+        .filter(
+          player =>
+            player.position === 'goalKeeper' &&
+            player.isConfirmed &&
+            player.confirmationDate,
+        )
+        .map(this.mapPlayersToViewModel)
+        .sort(
+          (a, b) =>
+            a.confirmationDate?.getTime()! - b.confirmationDate?.getTime()!,
+        ),
+      out: event.players
+        .filter(player => player.confirmationDate && !player.isConfirmed)
+        .map(this.mapPlayersToViewModel)
+        .sort(
+          (a, b) =>
+            a.confirmationDate?.getTime()! - b.confirmationDate?.getTime()!,
+        ),
+      players: event.players
+        .filter(
+          player =>
+            player.position !== 'goalKeeper' &&
+            player.isConfirmed &&
+            player.confirmationDate,
+        )
+        .map(this.mapPlayersToViewModel)
+        .sort(
+          (a, b) =>
+            a.confirmationDate?.getTime()! - b.confirmationDate?.getTime()!,
+        ),
+    };
+  }
+
+  private mapPlayersToViewModel(
+    player: NextEventPlayerEntity,
+  ): NextEventPlayerViewModel {
+    return new NextEventPlayerViewModel({
+      name: player.name,
+      position: player.position,
+      photo: player.photo,
+      initials: player.initials,
+      isConfirmed: player.isConfirmed,
+      confirmationDate: player.confirmationDate,
+    });
   }
 }
