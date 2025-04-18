7ffcce9b96e27bf21d40620f65e7bd5e9241345f
Junior Marques <brq.calmarques@voegol.com>
2025-04-18 08:53:33 -0300

    ensure NextEventPresenter return goolkeepers with correct data

--- src/__test__/presenter/next_event_presenter.test.ts
+++ src/__test__/presenter/next_event_presenter.test.ts
@@ -31,7 +31,18 @@ class NextEventPresenter implements INextEventPresenter {
         .filter(player => !player.confirmationDate)
         .map(this.mapPlayersToViewModel)
         .sort((a, b) => a.name.localeCompare(b.name)),
-      goalKeepers: [],
+      goalKeepers: event.players
+        .filter(
+          player =>
+            player.position === 'goalKeeper' &&
+            player.isConfirmed &&
+            player.confirmationDate,
+        )
+        .map(this.mapPlayersToViewModel)
+        .sort(
+          (a, b) =>
+            a.confirmationDate?.getTime()! - b.confirmationDate?.getTime()!,
+        ),
       out: event.players
         .filter(player => player.confirmationDate && !player.isConfirmed)
         .map(this.mapPlayersToViewModel)
@@ -213,5 +224,50 @@ describe('NextEventPresenter', () => {
     expect(response.out[0].name).toBe('Carla Doe');
     expect(response.out[1].name).toBe('Ana Doe');
     expect(response.out[2].name).toBe('Bia Doe');
+  });
+
+  it('should return goalkeepers list sorted by confirmation date', async () => {
+    const groupId = anyString();
+    const nextEventLoadedUseCaseSpy = new NextEventLoadedUseCaseSpy();
+    nextEventLoadedUseCaseSpy.response = {
+      groupName: 'test',
+      date: new Date(),
+      players: [
+        NextEventPlayerEntity.create({
+          id: '1',
+          name: 'Bia Doe',
+          isConfirmed: true,
+          photo: 'https://example.com/photo.jpg',
+          position: 'goalKeeper',
+          confirmationDate: new Date('2025-04-18T12:00:00'),
+        }),
+        NextEventPlayerEntity.create({
+          id: '2',
+          name: 'Ana Doe',
+          isConfirmed: false,
+          confirmationDate: new Date('2025-04-18T13:00:00'),
+        }),
+        NextEventPlayerEntity.create({
+          id: '2',
+          name: 'Ana Doe',
+          isConfirmed: true,
+          confirmationDate: new Date('2025-04-18T11:00:00'),
+          position: 'goalKeeper',
+        }),
+        NextEventPlayerEntity.create({
+          id: '3',
+          name: 'Carla Doe',
+          isConfirmed: true,
+          confirmationDate: new Date('2025-04-18T10:00:00'),
+        }),
+      ],
+    };
+
+    const {sut} = makeSut({nextEventLoadedUseCaseSpy});
+    const response = await sut.loadNextEvent({groupId});
+
+    expect(response.goalKeepers.length).toBe(2);
+    expect(response.goalKeepers[0].name).toBe('Ana Doe');
+    expect(response.goalKeepers[1].name).toBe('Bia Doe');
   });
 });
