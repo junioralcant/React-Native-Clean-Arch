80d8a2f979509d6829a50aec6e8690dbf4c76a9d
junioralcant <junioralcant111@gmail.com>
2025-08-21 09:54:55 -0300

    ensure LoadNextEventCacheRepository return NextEvent on success

--- src/__test__/infra/cache/repositories/load_next_event_cache_repo.test.ts
+++ src/__test__/infra/cache/repositories/load_next_event_cache_repo.test.ts
@@ -1,21 +1,49 @@
 import {expect, it, describe, beforeEach} from '@jest/globals';
 import {anyString} from '../../../helpers/fakes';
+import {NextEventEntity} from '../../../../domain/entities/next_event_';
+import {NextEventPlayerEntity} from '../../../../domain/entities/next_event_player';
+import {Json} from '../../../../infra/types/json';
 
 export type GetParams = {
   key: string;
 };
 
 export interface ICacheGetClient {
-  get(params: GetParams): Promise<void>;
+  get<T>(params: GetParams): Promise<T>;
 }
 
 export class CacheGetClientSpy implements ICacheGetClient {
   key? = '';
   callsCount = 0;
-  async get(params: GetParams): Promise<void> {
+  response: any = {};
+  async get<T>(params: GetParams): Promise<T> {
     this.key = params?.key;
     this.callsCount++;
+    return this.response;
   }
+}
+
+export function toNextEventEntity(response: Json): NextEventEntity {
+  return new NextEventEntity({
+    date: response.date,
+    groupName: response.groupName,
+    players: response.players.map(toNextEventPlayerEntity),
+  });
+}
+
+export function toNextEventPlayerEntity(
+  player: NextEventPlayerEntity,
+): NextEventPlayerEntity {
+  return NextEventPlayerEntity.create({
+    id: player.id,
+    name: player.name,
+    isConfirmed: player.isConfirmed,
+    photo: player.photo,
+    position: player.position,
+    confirmationDate: player.confirmationDate
+      ? new Date(player.confirmationDate)
+      : undefined,
+  });
 }
 
 type loadNextEventParams = {groupId: string};
@@ -25,10 +53,14 @@ export class LoadNextEventCacheRepository {
     private readonly cacheClient: ICacheGetClient,
     private readonly key: string,
   ) {}
-  async loadNextEvent({groupId}: loadNextEventParams): Promise<void> {
-    await this.cacheClient.get({
+  async loadNextEvent({
+    groupId,
+  }: loadNextEventParams): Promise<NextEventEntity> {
+    const response = await this.cacheClient.get<Json>({
       key: `${this.key}:${groupId}`,
     });
+
+    return toNextEventEntity(response);
   }
 }
 
@@ -42,7 +74,25 @@ describe('LoadNextEventCacheRepository', () => {
     groupId = anyString();
     key = anyString();
     cacheClient = new CacheGetClientSpy();
-
+    cacheClient.response = {
+      date: new Date('2024-01-01T10:30'),
+      groupName: 'any_name',
+      players: [
+        {
+          id: 'id 1',
+          name: 'name 1',
+          isConfirmed: true,
+        },
+        {
+          id: 'id 2',
+          name: 'name 2',
+          isConfirmed: false,
+          position: 'position 2',
+          confirmationDate: new Date('2024-01-01T10:30'),
+          photo: 'photo 2',
+        },
+      ],
+    };
     sut = new LoadNextEventCacheRepository(cacheClient, key);
   });
 
@@ -50,5 +100,25 @@ describe('LoadNextEventCacheRepository', () => {
     await sut.loadNextEvent({groupId});
     expect(cacheClient.key).toBe(`${key}:${groupId}`);
     expect(cacheClient.callsCount).toBe(1);
+  });
+
+  it('should request NextEvent on success', async () => {
+    const event = await sut.loadNextEvent({groupId});
+    expect(event.groupName).toBe('any_name');
+    expect(event.date).toStrictEqual(new Date('2024-01-01T10:30'));
+
+    expect(event.players[0].id).toBe('id 1');
+    expect(event.players[0].name).toBe('name 1');
+    expect(event.players[0].isConfirmed).toBe(true);
+
+    expect(event.players[1].id).toBe('id 2');
+    expect(event.players[1].name).toBe('name 2');
+    expect(event.players[1].position).toBe('position 2');
+    expect(event.players[1].photo).toBe('photo 2');
+
+    expect(event.players[1].confirmationDate).toStrictEqual(
+      new Date('2024-01-01T10:30'),
+    );
+    expect(event.players[1].isConfirmed).toBe(false);
   });
 });
